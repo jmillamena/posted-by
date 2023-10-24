8b27d5c0949c96d34a743eae205f5dc81b966ab3
Jmillamena <jcmillamena@gmail.com>
2023-10-24 07:46:09 -1000

    creates routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
+//Import Components
+import Navbar from "./Navbar";
+import Home from "./Home";
+import User from "./User";
+import ManageFriends from "./ManageFriend";
+import FriendProfiles from "./FriendProfiles";
+
 function App() {
-  return <h1>Project Client</h1>;
+  return;
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/users" component={User} />
+      <Route path="/friend-profiles" component={FriendProfiles} />
+      <Route path="/manage-friends" component={ManageFriends} />
+    </Switch>
+  </Router>;
 }
 
 export default App;
---
+++ server/migrations/versions/187573e48675_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 187573e48675
+Revises: 
+Create Date: 2023-10-24 06:46:01.446409
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '187573e48675'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
