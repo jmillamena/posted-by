b626de3cd9321b44066a1ea8b951c9e9df2269dc
Jmillamena <jcmillamena@gmail.com>
2023-10-25 10:41:24 -1000

    adds create post and view post routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 //Import Components
 import Navbar from "./Navbar";
@@ -9,16 +9,17 @@ import ManageFriends from "./ManageFriend";
 import FriendProfiles from "./FriendProfiles";
 
 function App() {
-  return;
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/users" component={User} />
-      <Route path="/friend-profiles" component={FriendProfiles} />
-      <Route path="/manage-friends" component={ManageFriends} />
-    </Switch>
-  </Router>;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/users" component={User} />
+        <Route path="/friend-profiles" component={FriendProfiles} />
+        <Route path="/manage-friends" component={ManageFriends} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/components/FriendProfiles.js
+++ client/src/components/FriendProfiles.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+import CreatePost from "./CreatePost";
+import ViewPostDetails from "./ViewPostDetails";
+
+const FriendProfiles = () => {
+  return (
+    <div>
+      {/* Friend Profiles content */}
+      <Switch>
+        <Route path="/friend-profiles/create-post" component={CreatePost} />
+
+        <Route
+          path="/friend-profiles/view-post-details"
+          component={ViewPostDetails}
+        />
+      </Switch>
+    </div>
+  );
+};
 
 export default FriendProfiles;
