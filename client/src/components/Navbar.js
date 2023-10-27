9d6f67976d55b1edd2150fa4df2a9542ed7e3982
Jmillamena <jcmillamena@gmail.com>
2023-10-27 09:01:15 -1000

    updates navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home Feed
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/profile" className="nav-link">
+            My Profile
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/friend-requests" className="nav-link">
+            Manage Friend Requests
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/friend-profile" className="nav-link">
+            Friend Profiles
+          </Link>
+        </li>
+        <ul className="sub-nav">
+          <li className="nav-item">
+            <Link to="/create-post" className="nav-link">
+              Create New Post
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/view-edit-post" className="nav-link">
+              View/Edit Post
+            </Link>
+          </li>
+        </ul>
+      </ul>
+    </nav>
+  );
+};
 export default Navbar;
